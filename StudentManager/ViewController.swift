830bb014f8df25234bcc1c154a071126be1f21dc
MedNizar <rockstarhard4@gmail.com>
2021-06-08 21:35:28 +0100

    Initial Commit

---
+++ StudentManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentManager
+//
+//  Created by MNizar on 6/7/21.
+//  Copyright © 2021 MNizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
