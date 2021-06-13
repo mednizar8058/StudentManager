475207160902d5778fe0e6b4e76e9238a2718dd6
MedNizar <rockstarhard4@gmail.com>
2021-06-13 15:44:57 +0100

    deleting some views and adding SettingsViewController

---
+++ StudentManager/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  StudentManager
+//
+//  Created by MNizar on 6/13/21.
+//  Copyright © 2021 MNizar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
